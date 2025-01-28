b00225bcced3973b8023656aeaf5af2f85bc7448
Tony Dyer <Zionkrill1@protonmail.com>
2025-01-28 23:47:30 +0000

    Ready for grading

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -6,9 +6,11 @@
 #
 
 # Variables to hold the sales total and the profit
-
-# Get the amount of projected sales.
-
+userval = float(input("Please input your projected sales! $ "))
+profit = 0.23
+# Get the amount of projected sales.       I'll be honest, these comments are really confusing for me in how im supposed to
+                                         # write this code 
 # Calculate the projected profit.
-
+totalP = profit * userval
 # Print the projected profit.
+print('Your projected profit is $',format(totalP,',.2f'))
