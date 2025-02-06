e96b069802ec79229cd1a706468e491857f33292
Tony Dyer <Zionkrill1@protonmail.com>
2025-02-06 01:14:19 +0000

    ready for grading

--- Areas-of-Rectangles/main.py
+++ Areas-of-Rectangles/main.py
@@ -7,15 +7,21 @@
 # Local variables
 
 # Get length A
-
+length1 = int(input("Enter the length of rectangle 1: "))
 # Get width A
-
+width1 = int(input("Enter the width of rectange 1: "))
 # Get length B
-
+length2 = int(input("Enter the length of rectangle 2: "))
 # Get width B
-
+width2 = int(input("Enter the width of rectangle 2: "))
 # Calculate area A
-
+area1 = length1 * width1
 # Calculate area B
-
+area2 = length2 * width2
 # Print area comparison using if-elif-else statements
+if area1 > area2:
+    print("Rectangle 1 has the greater area")
+elif area2 > area1:
+    print(" Rectangle 2 has the greater area")
+else:
+    print("Both have the same area")
