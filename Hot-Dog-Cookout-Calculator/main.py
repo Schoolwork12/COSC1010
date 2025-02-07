ceb8912d44631ceffc5e6ac52ad3531d1e2f9685
Tony Dyer <Zionkrill1@protonmail.com>
2025-02-07 23:59:35 +0000

    Ready for grading!

--- Hot-Dog-Cookout-Calculator/main.py
+++ Hot-Dog-Cookout-Calculator/main.py
@@ -1,29 +1,84 @@
 #
 # Name Tony Dyer
-# Date 2/5/2025
+# Date 2/7/2025
 # Hot Dog Cookout Calculator Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program.
 
-#get input from the user
-people = int(input("Enter the number of people attending the cookout: "))
-hotdogs_perp = int(input("Enter the number of hot dogs each person will get: "))
+# const
+HOT_DOGS_PER_PACKAGE = 10
+BUNS_PERPACKAGE = 8
 
-#calculate total hot dogs and buns needed
-totaldogs = people * hotdogs_perp
-totalb = people + hotdogs_perp
+# local vars
+numAttending = 0    # Number of people attending
+numPerPerson = 0    # num of hotdogs and buns per person
+total = 0   # the total number of hot dogs and buns needed
+minDogs = 0     # min number of packages of hot dogs
+minBuns = 0     # min number of packages for hot dog buns
+dogsLeft = 0    # number of hot dogs left over from a package
+bunsLeft = 0    # number of hot dog buns left over from a package
 
-#calculate the minimum number of packages needed
-hotdogp = (totaldogs + 9) // 10  #rounding to next package
-bunp = (totalb + 7) // 8  #rounding to next package
+# get the number of people attending
+numAttending = int(input("Enter the number of people attending the cookout:  "))
 
-#calculate leftovers
-leftoverdogs = (hotdogp * 10) - totaldogs
-leftoverb = (bunp * 8) - totalb
+# get the number of hot dogs per person
+numPerPerson = int(input(" Enter the number of hotdogs per person:  "))
 
-#display results
-print(f"Minimum number of packages of hot dogs required: {hotdogp}")
-print(f"Minimum number of packages of hot dog buns required: {bunp}")
-print(f"Number of hot dogs left over: {leftoverdogs}")
-print(f"Number of hot dog buns left over: {leftoverb}")
+# calc total the number of hotdogs and buns needed
+total = numAttending * numPerPerson
+
+# calc the min number of packages of hot dogs needed
+minDogs = total // HOT_DOGS_PER_PACKAGE 
+
+# determin if the number of people attending is large enough to req more than one package of hotdogs
+if minDogs > 0:
+    #calc the number of hotdogs left over from a package inf any
+    dogsLeft = total % HOT_DOGS_PER_PACKAGE
+
+    # if there will be left overs add additional package of hot dogs
+    if dogsLeft != 0:
+        minDogs += 1
+
+# set the min number of packages of hot dogs to one because the number of people attending is small enough to only need one package        
+else:
+    minDogs = 1
+
+
+# determine if the number of leftover hot dogs if any
+dogsLeft = (HOT_DOGS_PER_PACKAGE * minDogs) - total
+
+
+###### for Buns
+
+# calc the min number of packages of hot dog buns needed
+minBuns = total // BUNS_PERPACKAGE 
+
+# determin if the number of people attending is large enough to req more than one package of buns
+if minBuns > 0:
+    #calc the number of buns left over from a package inf any
+    bunsLeft = total % BUNS_PERPACKAGE
+
+    # if there will be left overs add additional package of buns
+    if bunsLeft != 0:
+        minBuns += 1
+
+# set the min number of packages of buns to one because the number of people attending is small enough to only need one package        
+else:
+    minBuns = 1
+
+
+# determine if the number of leftover buns if any
+bunsLeft = (BUNS_PERPACKAGE * minBuns) - total
+
+# display the min packages of hot dogs needed
+print('Minimum number of packages of hot dogs needed : ', minDogs)
+
+# display the min packages of buns needed
+print('Minimum number of packages of hot dog buns needed: ', minBuns)
+
+# display the number of left over hot dogs
+print('Hot dogs left over: ', dogsLeft)
+
+# display the number of left over hot dog buns
+print('Hot dog buns left over: ', bunsLeft)
