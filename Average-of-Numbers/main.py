c3a00ddacd5795a4aeaf2bed833f47765b7bafed
Tony Dyer <Zionkrill1@protonmail.com>
2025-03-11 00:03:05 +0000

    Ready for grading

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,30 @@
 #
-# Name
-# Date
+# Name Tony Dyer
+# Date 3/10/2025
 # Average of Numbers Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+# open the file
+num_file = open('numbers.txt', 'r')
+
+# accumulators
+total = 0
+amountofnumb = 0
+
+# read and process each line
+for line in num_file:
+    number = int(line.strip())  # convert line to integer + strip if needed
+    total += number  # add number to total
+    amountofnumb += 1  # count the number of entries
+
+# close the file
+num_file.close()
+
+# calculate and display the average
+if amountofnumb > 0:  # prevent division by zero just in case!
+    average = total / amountofnumb
+    print(f"Average: {average}")
+else:
+    print("The file is empty, cannot calculate an average.")
--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,16 @@
 #
-# Name
-# Date
+# Name Tony Dyer
+# Date 3/10/2025
 # File Display Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+# open the file
+num_file = open('numbers.txt', 'r')
+# read/display the files contents
+for line in num_file:
+    number = int(line)
+    print(number)
+#close the file
+num_file.close()
