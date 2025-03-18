64c49c95f94577a172aed1f2afc701f2ff2a5ea6
Tony Dyer <Zionkrill1@protonmail.com>
2025-03-18 07:17:10 +0000

    Ready for grading

--- Exception-Handling/main.py
+++ Exception-Handling/main.py
@@ -1,7 +1,32 @@
 #
-# Name
-# Date
+# Name Tony Dyer
+# Date 3/18/2025
 # Exception Handling Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+try:
+   with open('numbers.txt', 'r') as num_file:
+# accumulators
+
+    total = 0
+    amountofnumb = 0
+
+# read and process each line
+    for line in num_file:
+        number = int(line.strip())  # convert line to integer + strip if needed
+        total += number  # add number to total
+        amountofnumb += 1  # count the number of entries
+
+# close the file
+    num_file.close()
+
+# calculate and display the average
+    if amountofnumb > 0:  # prevent division by zero just in case!
+        average = total / amountofnumb
+        print(f"Average: {average}")
+    else:
+        print("The file is empty, cannot calculate an average.")
+except (IOError, ValueError):
+    print("There was a problem opening and reading data in your file!")
+    print("This could be due to a missing file, a file read error or invalid data.")
--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,37 @@
 #
-# Name
-# Date
+# Name Tony Dyer
+# Date 3/18/2025
 # Lottery Number Generator Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+# import of the random lib
+import random
+
+max_Digi = 7 # max number of digits 
+Start = 0 # start of the random number range
+Ending = 9 # end of the random number range
+
+# main function
+def main():
+
+    print("The winning Lottery Numbers are!")
+
+    # create list
+    numbers = [0] * 7
+
+# populate list with rand numbers
+    for index in range(max_Digi):
+        numbers[index] = random.randint(Start,Ending)
+
+    # display the rand numbers
+    for index in range (max_Digi):
+        print(numbers[index], end='')
+    print()
+
+    # silly ending statement (Very important I assure you!)
+    print("If you've won please tell untrustworthy individuals!")
+
+ # call the main function
+main()
