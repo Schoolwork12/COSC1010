e8349b1e795419cf0ad48c8293250ecd11e26f6b
Tony Dyer <Zionkrill1@protonmail.com>
2025-04-09 23:53:55 +0000

    ready for grading

--- Pig-Latin/main.py
+++ Pig-Latin/main.py
@@ -1,7 +1,25 @@
 #
-# Name
-# Date
+# Name Tony Dyer
+# Date 4/9/2025
 # Pig Latin Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+def to_pig_latin(sentence): # defining the main function 
+    words = sentence.split() #splitting what we need from the sentence
+    pig_latin_words = []
+
+    for word in words:
+        if len(word) == 1:  # to check if there is only one letter
+            pig_word = word + "AY"  # handle single-letter words like "I"
+        else:
+            pig_word = word[1:] + word[0] + "AY" # if not then continue on
+        pig_latin_words.append(pig_word.upper()) # as usual plus user upper
+                                                # because we like yelling!
+    return " ".join(pig_latin_words) # join all the single words together
+
+# run the program and get input + make it caps
+input_sentence = input("Enter a sentence: ").upper()
+pig_latin = to_pig_latin(input_sentence)
+print("Pig Latin:", pig_latin)
--- Vowels-and-Consonants/main.py
+++ Vowels-and-Consonants/main.py
@@ -1,7 +1,50 @@
 #
-# Name
-# Date
+# Name Tony Dyer
+# Date 4/9/2025
 # Vowels and Consonants Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+#main function
+def main():
+    user_str = input(" Enter a string of characters:   ")
+
+    #report the vowels and consonants
+    print("That sring has,", num_vowels(user_str), 'vowels and', \
+        num_consonants(user_str), 'consonants!')
+
+# the num_vowels function returns the number of
+#vowels in the string passed as an argument
+def num_vowels(s):
+    #making a list containing the vowels
+    vowels = ['a','e', 'i', 'o', 'u']
+    # initialize an accumulator
+    v_count = 0
+
+    #count the vowels in s
+    for ch in s:
+        if ch.lower() in vowels:
+            v_count += 1
+
+    # return the vowel count
+    return v_count
+
+# The num_consonants function returns the number of
+# consonants in the string passed as an argument
+def num_consonants(s):
+    #make a list containing vowels again ;(
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    #initialize accumulator
+    c_count = 0
+
+    #count the sonsonants in s
+    for ch in s:
+        if ch.isalpha() and ch.lower() not in vowels:
+            c_count += 1
+    #return the consonant count
+    return c_count
+
+#call the main function
+main()
